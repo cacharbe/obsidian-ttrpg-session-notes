3a97c4c89aea5fc907120286313f148cf7cceca3
cacharbe <cacharbe@users.noreply.github.com>
2024-08-13 18:22:34 +0000

    deploy: 9f252297ed88d82df9a59f48008c15d6a00de699

---
+++ main.ts
@@ -0,0 +1,42 @@
+import { Console } from 'console';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder } from 'obsidian';
+import CreateSessionModal from './CreateSessionModal';
+import CreateSessionSettingsTab from './CreateSessionSettings';
+
+interface CreateSessionPluginSettings {
+	sessionTemplate: string,
+	ttrpgsFolder: string,
+}
+const DEFAULT_SETTINGS: CreateSessionPluginSettings = {
+	sessionTemplate: '',
+	ttrpgsFolder: '',
+}
+
+export default class CreateSessionPlugin extends Plugin {
+	settings: CreateSessionPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		// This creates an icon in the left ribbon.
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Create Session', (evt: MouseEvent) => {
+			new CreateSessionModal(this.app, this).open();
+		});
+
+		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Add the settings tab
+		this.addSettingTab(new CreateSessionSettingsTab(this.app, this));
+		
+	}
+
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+
